2a0658ad2d1e2012f15af34fc2b17530cbc9b574
Stoyan Haydushki <s.haidushky@abv.bg>
2021-06-18 11:24:13 +0300

    Add some comments

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import socket
 
+# Define socket host and port
 HOST, PORT = '', 8888
 
+# Create the socket
 listenSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listenSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listenSocket.bind((HOST, PORT))
@@ -10,10 +12,15 @@ listenSocket.listen(1)
 # Output that the server is active
 print(f'Serving HTTP on port {PORT}...')
 while True:
+    # Wait for client connections
     clientConnection, clientAddress = listenSocket.accept()
-    requestData = clientConnection.recv(1024)
-    print(requestData.decode('utf-8'))
 
+    # Get the client request
+    requestData = clientConnection.recv(1024)
+    decodedData = requestData.decode('utf-8')
+    print(decodedData)
+
+    # Prepare and send the response
     httpResponse = b"""\
     HTTP/1.1 200 OK
 
