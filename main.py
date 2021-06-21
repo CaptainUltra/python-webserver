0384de4a1088a4083e322e7ee352e700696b9cfb
Stoyan Haydushki <s.haidushky@abv.bg>
2021-06-21 14:15:05 +0300

    Refactor code into separate functions; changed variable names according to style guide

--- main.py
+++ main.py
@@ -3,25 +3,15 @@ import socket
 # Define socket host and port
 HOST, PORT = '', 8888
 
-# Create the socket
-listenSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listenSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listenSocket.bind((HOST, PORT))
-listenSocket.listen(1)
 
-# Output that the server is active
-print(f'Serving HTTP on port {PORT}...')
-while True:
-    # Wait for client connections
-    clientConnection, clientAddress = listenSocket.accept()
-
+def handle_request(client_connection):
     # Get the client request
-    requestData = clientConnection.recv(1024)
-    decodedData = requestData.decode()
-    print(decodedData)
+    request_data = client_connection.recv(1024)
+    decoded_data = request_data.decode()
+    print(decoded_data)
 
     # Parse HTTP headers
-    headers = decodedData.split('\r\n')
+    headers = decoded_data.split('\r\n')
     filename = headers[0].split()[1]
 
     # Get the content of the file
@@ -33,11 +23,30 @@ while True:
         content = fin.read()
         fin.close()
 
-        httpResponse = 'HTTP/1.0 200 OK\n\n' + content
+        http_response = 'HTTP/1.0 200 OK\n\n' + content
     except FileNotFoundError:
 
-        httpResponse = 'HTTP/1.0 404 Not Found\n\nRequested file path is not valid.'
+        http_response = 'HTTP/1.0 404 Not Found\n\nRequested file path is not valid.'
 
     # Send the HTTP response
-    clientConnection.sendall(httpResponse.encode())
-    clientConnection.close()
+    client_connection.sendall(http_response.encode())
+
+
+def serve_forever():
+    # Create the socket
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listen_socket.bind((HOST, PORT))
+    listen_socket.listen(1)
+
+    # Output that the server is active
+    print(f'Serving HTTP on port {PORT}...')
+    while True:
+        # Wait for client connections
+        client_connection, client_address = listen_socket.accept()
+        handle_request(client_connection)
+        client_connection.close()
+
+
+if __name__ == '__main__':
+    serve_forever()
